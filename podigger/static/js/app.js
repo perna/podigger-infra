abac0b00fe74413553ad81451db42cde73fcbd21
Anderson Meira <andersonmeira@outlook.com>
2016-04-13 10:37:10 -0300

    mudanças na estrutura e layout do projeto

--- podigger/static/js/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('podigger',['ngRoute'])
-    .config(function($routeProvider) {
-
-        $routeProvider
-            .when('/', {
-                templateUrl: 'static/partials/search.html'
-            });
-    });
